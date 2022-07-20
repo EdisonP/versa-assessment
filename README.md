28a4d57cd9ec21246c97920a471387d4454c0c21
EdisonP <edisonphang@gmail.com>
2022-07-20 22:36:38 +0800

    Functional promise for rateUSDMYR
    Successfully loaded rateUSDMYR structurally via promise.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Be sure to navigate into this project's folder in the terminal.
 
+Execute "npm install" to initialize node_modules
+
 # Challenge 1
 
 Execute the file c1.js with "node c1.js" in your terminal (CMD.exe, Visual Studio Code...) to produce output.js with the expected output.
--- c2.js
+++ c2.js
@@ -2,32 +2,110 @@ const axios = require("axios");
 const symbolArray = ["BTC", "ETH", "XRP"];
 const currencyArray = ["USD", "MYR"];
 
-function getCrypto(symbol, currency) {
-  const output = {
-    method: "GET",
-    url: "https://alpha-vantage.p.rapidapi.com/query",
-    params: {
-      from_currency: symbol,
-      function: "CURRENCY_EXCHANGE_RATE",
-      to_currency: currency,
-      datatype: "json",
-      output_size: "compact",
-    },
-    headers: {
-      "X-RapidAPI-Key": "e79d7e9983mshc39d988d165c7ccp17a614jsn24f04095fb67",
-      "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
-    },
-  };
+const api_url = "https://alpha-vantage.p.rapidapi.com/query";
+const api_key = "e79d7e9983mshc39d988d165c7ccp17a614jsn24f04095fb67";
+const api_host = "alpha-vantage.p.rapidapi.com",
+  finalOutput = [];
 
-  axios
-    .request(output)
-    .then(function (response) {
-      console.log(response.data);
+function getCrypto(symbolTarget) {
+  var priceUSD_res;
+  var priceMYR_res;
+  var rateUSDMYR_res;
+  var date;
+  var finalRes = {};
+
+  getRate()
+    .then((rateUSDMYR_return) => {
+      rateUSDMYR_res = rateUSDMYR_return;
+      try {
+        // fetch currency rate
+        //fetch cryptodata
+
+        for (let i = 0; i < currencyArray.length; i++) {
+          const req = {
+            method: "GET",
+            url: api_url,
+            params: {
+              from_currency: symbolTarget,
+              function: "CURRENCY_EXCHANGE_RATE",
+              to_currency: currencyArray[i],
+              datatype: "json",
+              output_size: "compact",
+            },
+            headers: {
+              "X-RapidAPI-Key": api_key,
+              "X-RapidAPI-Host": api_host,
+            },
+          };
+          // send request to API
+          axios
+            .request(req)
+            .then(function (response2) {
+              res = response2.data[Object.keys(response2.data)[0]];
+              let currency = JSON.stringify(res[Object.keys(res)[2]]);
+              let date = JSON.stringify(res[Object.keys(res)[5]]);
+              if (currency.toString == currencyArray[0].toString) {
+                priceUSD_res = res[Object.keys(res)[4]];
+              } else if (currency.toString == currencyArray[1].toString) {
+                priceMYR_res = res[Object.keys(res)[4]];
+              }
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        }
+      } catch (error) {
+        console.log(error);
+      }
     })
-    .catch(function (error) {
-      console.error(error);
+    .finally(() => {
+      finalRes = {
+        symbol: symbolTarget,
+        priceUSD: priceUSD_res,
+        priceMYR: priceMYR_res,
+        rateUSDMYR: rateUSDMYR_res,
+        date: "bruh",
+      };
+      console.log(finalRes);
     });
 }
 
-// async to execute all getCrypto()
-getCrypto("BTC", "USD");
+function getRate() {
+  return new Promise(function (resolve, reject) {
+    const currencyRate = {
+      method: "GET",
+      url: api_url,
+      params: {
+        to_currency: currencyArray[1],
+        function: "CURRENCY_EXCHANGE_RATE",
+        from_currency: currencyArray[0],
+      },
+      headers: {
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": api_host,
+      },
+    };
+    axios
+      .request(currencyRate)
+      .then(function (response) {
+        var res = response.data[Object.keys(response.data)[0]];
+        rateUSDMYR_res = res[Object.keys(res)[8]];
+        if (rateUSDMYR_res != null) {
+          resolve(rateUSDMYR_res);
+        } else {
+          reject("Error fetching from API");
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
+}
+
+function run() {
+  // 3 requests per symbol
+  getCrypto(symbolArray[0]);
+  // console.log(output);
+}
+
+run();
